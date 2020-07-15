e48b3b1bee4699bb92b86123c7195f1354a83535
Goutham-CodeBreaker <gouthamd.btech@gmail.com>
2020-07-15 13:11:40 +0530

    new branch1 commit

--- SampleProject/src/main/java/com/eoxys/main/SampleProject.java
+++ SampleProject/src/main/java/com/eoxys/main/SampleProject.java
@@ -1,9 +1,13 @@
 package com.eoxys.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages="com.eoxys")
 public class SampleProject {
 
 	public static void main(String[] args) {
